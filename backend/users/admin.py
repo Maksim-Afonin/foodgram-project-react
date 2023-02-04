22539d1392a76d6b0109ec92e3045bf2a672d7e4
Maksim <webmaster.bryansk@yandex.ru>
2023-02-04 18:58:07 +0300

    gigjhr

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
+from rest_framework.authtoken.models import Token
 
 from users.models import Follow, User
 
 
 admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ['key', 'user']
+    search_fields = ['key', 'user__username']
+    ordering = ['-created']
+    verbose_name = 'Токен'
+    verbose_name_plural = 'Токены'
 
 
 @admin.register(User)
